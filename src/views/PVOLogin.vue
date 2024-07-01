0d6dfbb35830b192f33487d9e41c07360d107f9f
Marco45718 <mjuarez@adere.so>
2024-07-01 14:31:26 -0500

    Added correct design

---
+++ src/views/PVOLogin.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="login">
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.login {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.login-form {
+    background-color: #f0f0f0;
+}
+
+.login-image {
+    background-color: red;
+}
+
+</style>
