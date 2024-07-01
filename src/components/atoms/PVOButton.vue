9cb9312522f22d84ee7ae3e9685c2d6b3bb36e73
Marco45718 <mjuarez@adere.so>
2024-06-30 22:28:30 -0500

    Added multiple reusable components

---
+++ src/components/atoms/PVOButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-btn v-if="icon" icon>
+      <v-icon>{{ icon }}</v-icon>
+    </v-btn>
+    <v-btn v-else>
+      {{ text }}
+    </v-btn>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    icon: {
+      type: String,
+      required: false,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+  });
+  </script>
---
+++ src/components/organisms/PVOHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-toolbar image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+  
+      <v-spacer></v-spacer>
+  
+      <TestButton
+        v-for="(btn, index) in buttons"
+        :key="index"
+        :icon="btn.icon"
+        @click="btn.action"
+      />
+    </v-toolbar>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  import TestButton from '../atoms/TestButton.vue';
+  
+  const props = defineProps({
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    buttons: {
+      type: Array,
+      required: true,
+    },
+  });
+  </script>
