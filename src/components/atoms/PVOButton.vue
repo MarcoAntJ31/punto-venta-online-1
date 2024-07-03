b9fa8b9f10a015960372cc70f157886bf4e0f6cc
Marco45718 <mjuarez@adere.so>
2024-07-02 20:19:20 -0500

    Fix colors in buttbutton

--- src/components/atoms/PVOButton.vue
+++ src/components/atoms/PVOButton.vue
@@ -1,116 +1,85 @@
 <template>
-  <v-btn v-if="icon" icon>
-    <v-icon>{{ icon }}</v-icon>
-  </v-btn>
-  <v-btn
-    v-else
-    :color="aButtonColor"
-    :variant="variant"
-    :size="size"
-    :loading="loading"
-  >
-    {{ text }}
-  </v-btn>
-</template>
+    <v-btn v-if="icon" icon>
+      <v-icon>{{ icon }}</v-icon>
+    </v-btn>
+    <v-btn v-else
+        :color="aButtonColor"
+        :variant="variant" 
+        :size="size"
+    >
+      {{ text }}
+    </v-btn>
+  </template>
 
 <script lang="ts">
-export type AButtonColors =
-  | "base"
-  | "blue"
-  | "purple"
-  | "green"
-  | "red"
-  | "yellow";
-export type AButtonSizes =
-  | "x-small"
-  | "small"
-  | "default"
-  | "large"
-  | "x-large";
-export type AButtonVariants =
-  | "text"
-  | "flat"
-  | "elevated"
-  | "tonal"
-  | "outlined"
-  | "plain"
-  | "radio";
+
+export type AButtonColors = 'base' | 'blue' | 'purple' | 'green' | 'red' | 'yellow'
+export type AButtonSizes = 'x-small' | 'small' | 'default' | 'large' | 'x-large'
+export type AButtonVariants = 'text' | 'flat' | 'elevated' | 'tonal' | 'outlined' | 'plain' | 'radio'
+
 </script>
+  
+  <script lang="ts" setup>
+  import { defineProps, computed, PropType } from 'vue';
+  import { usePreferredColorScheme } from '@vueuse/core';
+  import { useTheme } from 'vuetify';
+  
+  const props = defineProps({
+    color: {
+        type: String as PropType<AButtonColors>,
+        default: 'base'
+    },
+    variant: {
+        type: String as PropType<AButtonVariants>,
+        default: 'flat'
+    },
+    size: {
+        type: String as PropType<AButtonSizes>,
+        default: 'default'
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+  });
 
-<script lang="ts" setup>
-import { usePreferredColorScheme } from "@vueuse/core";
-import { PropType, defineProps, computed } from "vue";
-import { useTheme } from "vuetify";
+  const theme = useTheme();
+  const preferredColor = usePreferredColorScheme();
 
-const props = defineProps({
-  color: {
-    type: String as PropType<AButtonColors>,
-    default: "base",
-  },
-  variant: {
-    type: String as PropType<AButtonVariants>,
-    default: "flat",
-  },
-  size: {
-    type: String as PropType<AButtonSizes>,
-    default: "default",
-  },
-  icon: {
-    type: String,
-    required: false,
-  },
-  text: {
-    type: String,
-    required: false,
-  },
-  loading: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const theme = useTheme();
-const preferredColor = usePreferredColorScheme();
-
-const inDarkMode = computed(() => {
-  return theme.global.current.value.dark || preferredColor.value === 'dark';
+  const inDarkMode = computed(() => {
+  return theme.global.current.value.dark || preferredColor.value === 'light';
 });
 
 const aButtonColor = computed(() => {
   if (props.disabled) {
-    return inDarkMode.value ? "#444445" : "#E4E7E9";
+    return inDarkMode.value ? '#444445' : '#E4E7E9';
   }
 
   switch (props.color) {
-    case "base":
-      if (props.variant === "outlined" || props.variant === "text") {
-        return "#6B6C6D";
+    case 'base':
+      if (props.variant === 'outlined' || props.variant === 'text') {
+        return inDarkMode.value ? '#6B6C6D' : '#6B6C6D'; // Colores para variantes en modo claro
       }
-      return inDarkMode.value ? "#202121" : "#F7F9FA";
-    case "blue":
-      return inDarkMode.value ? "#1E3A8A" : "#85C1E9"; // Azul en modo oscuro
-    case "purple":
-      return inDarkMode.value ? "#5B21B6" : "#AF7AC5";
-    case "green":
-      return inDarkMode.value ? "#166534" : "#65D740";
-    case "red":
-      return inDarkMode.value ? "#B91C1C" : "#FF4040";
-    case "yellow":
-      if (props.variant === "outlined" || props.variant === "text") {
-        return inDarkMode.value ? "#854D0E" : "#C37500";
+      return inDarkMode.value ? '#202121' : '#F7F9FA';
+    case 'blue':
+      return inDarkMode.value ? '#212F49' : '#85C1E9'; // Azul en modo claro
+    case 'purple':
+      return inDarkMode.value ? '#5B21B6' : '#AF7AC5';
+    case 'green':
+      return inDarkMode.value ? '#166534' : '#65D740';
+    case 'red':
+      return inDarkMode.value ? '#B91C1C' : '#FF4040';
+    case 'yellow':
+      if (props.variant === 'outlined' || props.variant === 'text') {
+        return inDarkMode.value ? '#854D0E' : '#C37500';
       }
-      return inDarkMode.value ? "#D97706" : "#F4D03F";
+      return inDarkMode.value ? '#D97706' : '#F4D03F';
     default:
-      return inDarkMode.value ? "#202121" : "#F7F9FA"; // Valor por defecto en modo oscuro
+      return inDarkMode.value ? '#202121' : '#F7F9FA'; // Valor por defecto en modo claro
   }
 });
-</script>
-
-<style scoped>
-:not(:disabled).a-button-blue,
-:not(:disabled).a-button-purple,
-:not(:disabled).a-button-green,
-:not(:disabled).a-button-red {
-  color: #ffffff;
-}
-</style>
+  </script>
